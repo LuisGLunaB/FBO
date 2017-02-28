9a7d1d8a932a383ddff0f256f553c4883edc7c55
LuisGLunaB <luis@mkti.mx>
2017-02-28 16:26:55 -0600

    Error handling, refactoring and some commenting.

--- FBO.php
+++ FBO.php
@@ -1,18 +1,27 @@
 <?php
 class Facebook{
+  /* THIS OBJECT FACILITATES THE USE OF FACEBOOK'S GRAPH API'S MAIN FUNCTIONS
+    MOST OF THE CODE DEALS WITH EXCEPTIONS, TOKEN REFRESHING AND DATA PARSING
+    EVERY MAIN QUERY HAS DEFAULT FIELDS TO BE PASSED TO FACEBOOK
+    EVERY QUERY IS PARSED IN ORDER TO HAVE AN STRUCTURED ASSOCIATIVE ARRAY
+    WITH NO MISSING VALUES AT ALL (BUT WITH NULLS VALUES).*/
+  protected $app_id = '632416283438924';
+  protected $app_secret = 'c478fbfb19e9f22f688b5db5144086c7';
+
+  # Default attribute values
   public $FB = NULL;
   public $status = True;
   public $message = "";
-  protected $app_id = '632416283438924';
-  protected $app_secret = 'c478fbfb19e9f22f688b5db5144086c7';
+  public $Token = NULL;
+  public $datos = array();
+  public $saveTokenWith = "COOKIE"; // Where to save FB's Token
+
+  # Permissions to be requested to the user
   public $permissions = ['public_profile','email','user_likes','user_friends',
   'pages_show_list','read_page_mailboxes','read_insights',
   'manage_pages','publish_pages','pages_messaging'];
 
-  public $Token = NULL;
-  public $saveTokenWith = "COOKIE";
-  public $datos = array();
-
+  # Platform methods
   public function __construct($Token = NULL, $FB = NULL){
     # Start Framework and Session if needed
     if (session_status() == PHP_SESSION_NONE) {session_start();}
@@ -20,7 +29,9 @@ class Facebook{
     require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
 
     # Connect to Facebook is $FB is not given
-    if(is_null($FB)){ $this->CONNECT(); }
+    if(is_null($FB)){
+      $this->CONNECT();
+    }
 
     # Manage Token: Catch or load Token if Token is not given
     if( is_null($Token) ){
@@ -29,95 +40,124 @@ class Facebook{
         $this->refreshToken();
         $this->saveToken();
       }
-      $this->loadToken();
+    $this->loadToken();
     }
 
   }
+  public function processError($message, $e = NULL, $exit = False){
+    # Every error in this Object leads here.
 
-  public function CONNECT(){
-    $this->FB = new Facebook\Facebook([
-      'app_id' => $this->app_id,
-      'app_secret' => $this->app_secret,
-      'default_graph_version' => 'v2.8']);
+    # Concatenate message and set status to False
+    $emessage = ( is_null($e) ) ? "" : $e->getMessage();
+    $this->status = False;
+    $this->message = $message . " " . $emessage;
+
+    if($exit){exit;} # exit execution or not
   }
+  public function CONNECT(){
+    # Connect to FB with the credentials
+    try{
+      $this->FB = new Facebook\Facebook([
+        'app_id' => $this->app_id,
+        'app_secret' => $this->app_secret,
+        'default_graph_version' => 'v2.8']);
+    }catch(Exception $e){
+      $this->processError("Error en CONNECT.",$e);
+    }
+  }
+
+  # Token handling methods
   public function catchToken(){
     $fb = &$this->FB;
   	$helper = $fb->getRedirectLoginHelper();
+
+    # Try to get a new Token
   	try {
   	  $Token = $helper->getAccessToken();
   	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-      $this->status = False;
-  	  $this->message = 'Graph tuvo un error al recuperar Token: ' . $e->getMessage();
-  	  exit;
+      $this->processError("Error al recuperar el Token en catchToken.", $e);
   	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-      $this->status = False;
-  	  $this->message = 'Facebook SDK tuvo un error: ' . $e->getMessage();
-  	  exit;
+      $this->processError("Error del SDK en catchToken.", $e);
   	}
 
+    # If you fail, set Token to NULL
   	if( isset($Token) ){
       $this->Token = $Token;
-  	}
+  	}else{
+      $this->Token = NULL;
+    }
+
   	return $this->Token;
   }
   public function refreshToken(){
     $fb = &$this->FB;
     $Token = &$this->Token;
+    $longLivedAccessToken = NULL;
 
-  	$oAuth2Client = $fb->getOAuth2Client();
-  	$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($Token);
-    $Token = $longLivedAccessToken;
-  	return (string) $longLivedAccessToken;
+    # Try to enlongate Token
+    if( !is_null($Token) ){
+        try{
+        	$oAuth2Client = $fb->getOAuth2Client();
+        	$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($Token);
+        }catch(Exception $e){
+          $this->processError("Error en refreshToken.",$e);
+        }
+        $Token = $longLivedAccessToken;
+    }else{
+        $this->processError("No hay Token previo en refreshToken.");
+    }
+
+  	return $longLivedAccessToken;
   }
   public function saveToken( $with = NULL ){
-    $with = ( is_null($with) ) ? $this->saveTokenWith : $with;
+    # Save Token depending on the method
     $Token = $this->Token;
 
-    # COOKIE
-    $_COOKIE["FBTOKEN"] = $Token;
-    setcookie("FBTOKEN", $Token, time() + 3600*24*30 ); //1 month
+    // Get saving method
+    $with = ( is_null($with) ) ? $this->saveTokenWith : $with;
+
+    # If saving with a COOKIE then:
+    if($with=="COOKIE"){
+      $_COOKIE["FBTOKEN"] = $Token;
+      setcookie("FBTOKEN", $Token, time() + 3600*24*30 ); //1 month
+    }
 
   }
   public function loadToken( $with=NULL){
+    # Load Token depending on the method
+    // Get saving method
     $with = ( is_null($with) ) ? $this->saveTokenWith : $with;
 
-    # COOKIE
-    $this->Token = ( isset($_COOKIE["FBTOKEN"]) ) ? $_COOKIE["FBTOKEN"] : NULL;
-  }
-  public function getLogin($URL = "", $relative = True ){
-    $fb = &$this->FB;
-    $permissions = &$this->permissions;
-
-    $URL = ($relative) ? "http://$_SERVER[HTTP_HOST]/$URL" : "$URL";
-  	$helper = $fb->getRedirectLoginHelper();
-  	$loginUrl = $helper->getLoginUrl($URL, $permissions);
-
-  	return htmlspecialchars($loginUrl);
+    # Loading from a COOKIE
+    if($with=="COOKIE"){
+      $this->Token = ( isset($_COOKIE["FBTOKEN"]) ) ? $_COOKIE["FBTOKEN"] : NULL;
+    }
   }
   public function hasToken(){
+    # Return if Object has a Token
     return ( !is_null($this->Token) );
   }
 
-  public static function issetKey($var,$key){
-    if( isset($var[$key]) ){
-      return $var[$key];
-    }else{
-      return NULL;
+  public function getLogin($URL = "", $relative = True ){
+    # Get Login URL for the user to grant access to Facebook
+    $fb = &$this->FB;
+    $permissions = &$this->permissions;
+
+    # Build Callback URL
+    $URL = ($relative) ? "http://$_SERVER[HTTP_HOST]/$URL" : "$URL";
+
+    # Try to request Login URL
+    try{
+    	$helper = $fb->getRedirectLoginHelper();
+    	$loginUrl = $helper->getLoginUrl($URL, $permissions);
+    }catch(Exception $e){
+      $this->processError("Error en getLogin.",$e);
     }
-  }
-  public static function parseNULL($key,&$parsed,&$datos){
-    $parsed["$key"] = ( isset($datos["$key"]) ) ? $datos["$key"] : NULL;
-  }
-  public static function parseBoolean($key,&$parsed,&$datos){
-    $x = ( isset($datos["$key"]) ) ? $datos["$key"] : NULL;
-    if( $x=="true" or $x=="True" or $x=="TRUE"){ $x = 1; }
-    if( $x=="false" or $x=="False" or $x=="FALSE" ){ $x = 0; }
-    $parsed["$key"] = $x;
-  }
-  public static function parse2Keys($key1,$key2,&$parsed,&$datos){
-    $parsed["$key1"."_$key2"] = ( isset($datos[$key1][$key2]) ) ? $datos[$key1][$key2] : NULL;
+
+  	return htmlspecialchars($loginUrl);
   }
 
+  # Main Parsing methods
   public static function parsePAGE( $datos ){
     $parsed = array();
 
@@ -185,26 +225,57 @@ class Facebook{
     return $parsed;
   }
 
+  # Secondary Parsing methods
+  public static function parseNULL($key,&$parsed,&$datos){
+    # Check if a key exists: return its Value or NULL
+    # Add the key back to $parsed (by reference)
+    $parsed["$key"] = ( isset($datos["$key"]) ) ? $datos["$key"] : NULL;
+  }
+  public static function parseBoolean($key,&$parsed,&$datos){
+    # Check if key exists: return NULL or Boolean to $parsed (by reference)
+    $x = ( isset($datos["$key"]) ) ? $datos["$key"] : NULL;
+    if( $x=="true" or $x=="True" or $x=="TRUE"){ $x = 1; }
+    if( $x=="false" or $x=="False" or $x=="FALSE" ){ $x = 0; }
+    $parsed["$key"] = $x;
+  }
+  public static function parse2Keys($key1,$key2,&$parsed,&$datos){
+    # Check if array with 2 given keys exists: return its Value or NULL (by reference)
+    $parsed["$key1"."_$key2"] = ( isset($datos[$key1][$key2]) ) ? $datos[$key1][$key2] : NULL;
+  }
+  public static function issetKey($var,$key){
+    # Check if an associative array has a key declared and return its Value.
+    if( isset($var[$key]) ){
+      return $var[$key];
+    }else{
+      return NULL;
+    }
+  }
+
+  # Data retriving methods
   public function GET($get){
   	$fb = &$this->FB;
     $Token = &$this->Token;
     $datos = &$this->datos;
 
-  	if( !is_null($Token) ){ $fb->setDefaultAccessToken($Token); }
-  	//$fb->setDefaultAccessToken($Token);
+    try{
+      $fb->setDefaultAccessToken($Token);
+    }catch(Exception $e){
+      $this->processError("Error al dar Token en GET.",$e);
+    }
+
   	try {
   	  $response = $fb->get($get);
   	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-      $this->status = False;
-  	  $this->message = 'Graph tuvo un error: ' . $e->getMessage();
-  	  //exit;
+      $this->processError("Error de Respuesta en GET.",$e);
   	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-      $this->status = False;
-  	  $this->message = 'Facebook SDK tuvo un error: ' . $e->getMessage()."<br />";
-  	  //exit;
+      $this->processError("Error al SDK en GET.",$e);
   	}
 
-  	if($this->status){$datos = $response->getDecodedBody();}
+    try{
+      $datos = $response->getDecodedBody();
+    }catch(Exception $e){
+      $this->processError("Error al crear array en GET.",$e);
+    }
 
   	return $datos;
   }
@@ -221,12 +292,25 @@ class Facebook{
     $fields = implode(",",$fields);
 
     $query = "$page?fields=$fields";
-    $this->GET($query);
-    $this->datos = $this->parsePAGE( $this->datos );
+
+    try{
+      $this->GET($query);
+      $this->datos = $this->parsePAGE( $this->datos );
+    }catch(Exception $e){
+      $this->processError("Error en PAGE.",$e);
+    }
+
   }
   public function PAGETOKEN($page){
     $query = "$page?fields=access_token";
-    return $this->GET($query);
+
+    try{
+      $this->GET($query);
+    }catch(Exception $e){
+      $this->processError("Error en PAGETOKEN.",$e);
+    }
+
+    return $this->datos;
   }
   public function ACCOUNTS( $access_token = False ){
     $fields = ["id","name","perms","username","instagram_accounts","picture","cover"];
@@ -234,11 +318,18 @@ class Facebook{
     $fields = implode(",",$fields);
 
     $query = "me/accounts?fields=$fields";
-    $this->GET($query);
-    $this->datos =$this->datos["data"];
-    $this->datos = $this->parseACCOUNT( $this->datos );
+
+    try{
+      $this->GET($query);
+      $this->datos =$this->datos["data"];
+      $this->datos = $this->parseACCOUNT( $this->datos );
+    }catch(Exception $e){
+      $this->processError("Error en ACCOUNTS.",$e);
+    }
+
   }
 
+  # Data displaying methods
   public function json($show=True,$isJSON=True){
 		$datos['status'] = $this->status;
 		$datos['message'] = $this->message;
@@ -248,11 +339,13 @@ class Facebook{
 		if($show){echo $json;}
 		return $json;
 	}
-  public function show(){
+  public function flush(){
+    $status = ($this->status) ? "True" : "False";
+    echo "status: ". $status."</br>";
+    echo "message: ". $this->message."</br>";
     print_r($this->datos);
-    echo $this->status;
-    echo $this->message;
   }
+
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
+# Report all PHP Errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
+# Include and declare FB Object
 include_once("FBO.php");
-
 $Face = new Facebook();
 
+#Set default Page to mkti.mx if none given
 $page = ( isset($_GET["page"]) ) ? $_GET["page"] : "mkti.mx";
 
+# If I have a Token, use it...
 if( $Face->hasToken() ){
   $Face->PAGE( $page );
-  //$Face->ACCOUNTS();
+  //$Face->ACCOUNTS( );
   $Face->json();
 }else{
+  # ...if not, request access.
   $loginURL = $Face->getLogin();
-  echo '<a href="' .($loginURL). '">Entra con Facebook!</a>';
+  echo '<a href="' .($loginURL). '">Entra con Facebook</a>';
 }
-
-
 
 
 ?>
